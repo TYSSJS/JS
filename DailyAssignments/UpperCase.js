fbdc1b06fbd934d61ad2049d42045bc7198a7017
praveen <gpraveenkumar001@gmail.com>
2019-09-15 21:48:04 +0530

    Done

---
+++ DailyAssignments/UpperCase.js
@@ -0,0 +1,5 @@
+/*Trying all the possible ways to use toUpperCase method */
+var str='javascript';
+console.log(str.toUpperCase(1));//JAVASCRIPT
+console.log(str.toUpperCase('0'));//JAVASCRIPT
+console.log(str.toUpperCase(true));//JAVASCRIPT
