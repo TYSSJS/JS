a27c4a576983d5db025ba79df504fa69fbe60d4b
chansmile2011 <chan.smile2011@gmail.com>
2019-09-16 14:23:16 +0530

    Sort added

---
+++ sortobj.js
@@ -0,0 +1,101 @@
+/*1.var arr=[2,1.5,5,5,,15,1002,5.5,12]
+
+console.log("Normal array: "+arr);
+console.log("Sorted array :"+arr.sort());
+
+"C:\Program Files\nodejs\node.exe" C:\Users\saritha\Desktop\Webstorm\JS\sortobj.js
+Normal array: 2,1.5,5,5,,15,1002,5.5,12
+Sorted array :1.5,1002,12,15,2,5,5,5.5,
+*/
+/*
+var arr=[-3,0,-5,-1,-100]
+
+console.log("Normal array: "+arr);
+console.log("Sorted array :"+arr.sort());
+
+Normal array: -3,0,-5,-1,-100
+Sorted array :-1,-100,-3,-5,0
+
+*/
+/*
+var arr=[2,1.5,5,5,15,1002,5.5,12]
+console.log("Normal array:"+arr)
+function mysort(a,b)
+{
+ return  a-b;
+}
+
+console.log(arr.sort(mysort));
+console.log(arr);
+
+*/
+/*
+var arr=[-3.2,0.1,-5.1,-1,-100,-1.3]
+console.log("Normal array:"+arr)
+function mysort(a,b)
+{
+    return  a-b;
+}
+
+console.log(arr.sort(mysort));
+console.log(arr);
+
+Normal array:-3.2,0.1,-5.1,-1,-100,-1.3
+    [ -100, -5.1, -3.2, -1.3, -1, 0.1 ]
+    [ -100, -5.1, -3.2, -1.3, -1, 0.1 ]
+*/
+/*
+var arr=["123",1,"5",123]
+console.log(arr.sort());
+console.log(arr);
+[ 1, '123', 123, '5' ]
+    [ 1, '123', 123, '5' ]
+
+*/
+/*
+
+var arr=["123",1,"5",123]
+console.log("Normal array:"+arr)
+function mysort(a,b)
+{
+    return  a-b;
+}
+
+console.log(arr.sort(mysort));
+console.log(arr);
+Normal array:123,1,5,123
+    [ 1, '5', '123', 123 ]
+    [ 1, '5', '123', 123 ]
+
+
+*/
+/*
+var arr=["123",1,undefined,"5",123,null]
+console.log(arr.sort());
+console.log(arr);
+[ 1, '123', 123, '5', null, undefined ]
+    [ 1, '123', 123, '5', null, undefined ]
+
+*/
+
+/*
+var arr=["123",1,undefined,"5",123,null]
+console.log("Normal array:"+arr)
+function mysort(a,b)
+{
+    return  a-b;
+}
+Normal array:123,1,,5,123,
+    [ null, 1, '5', '123', 123, undefined ]
+        [ null, 1, '5', '123', 123, undefined ]
+
+
+console.log(arr.sort(mysort));
+console.log(arr);
+*/
+
+/*
+var x=new object
+console.log(typeof(x));
+*/
+
